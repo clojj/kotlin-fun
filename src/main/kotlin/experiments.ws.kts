aad1e3bc7f5eaf9044c9a9b43e1d38fd2bf68ca3
clojj <jwin1968@gmail.com>
2019-12-17 23:24:39 +0100

    - Seiteneffekt entfernt - Pair Typ und Pair-deconstruction

--- src/main/kotlin/experiments.ws.kts
+++ src/main/kotlin/experiments.ws.kts
@@ -1,19 +1,23 @@
 class Cafe {
-
-    fun buyCoffee(cc: CreditCard, p: Payments): Coffee {
-        val cup = Coffee(name = "Filterbruehe", price = 2.50)
-        p.charge(cc, cup.price) // Seiteneffekt! ...testbar nur mit Mocks! wiederverwendbare Einheit? separation-of-concerns?
-        return cup
+    fun buyCoffee(cc: CreditCard): Pair<Coffee, Charge> {
+        val cup = Coffee("Wiener Melange", 3.5)
+        return Pair(cup, Charge(cc, cup.price))
     }
 }
 
 class Payments {
 
-    fun charge(cc: CreditCard, price: Double) {
-        println("charging $cc with $price")
-        val fees = price * 2 / 100
-        println("charging fees of $fees")
+    fun charge(charge: Charge) {
+        TODO("charge!")
     }
+}
+
+data class Charge(val cc: CreditCard, val amount: Double) {
+
+    fun combine(other: Charge): Charge =
+        if (cc == other.cc)
+            Charge(cc, amount + other.amount)
+        else throw Exception("Cannot combine charges to different cards")
 }
 
 data class CreditCard(val number: String)
@@ -24,8 +28,10 @@ data class Coffee(val name: String, val price: Double)
 
 val cafe = Cafe()
 val creditCard = CreditCard("1234567890")
-val payments = Payments()
 
-val coffee = cafe.buyCoffee(creditCard, payments)
+fun test() {
+    val (coffee, charge) = cafe.buyCoffee(creditCard)
+    println("$coffee $charge")
+}
 
-val coffees: List<Coffee> = List(5) { cafe.buyCoffee(creditCard, payments) } // nicht gut... 5 mal Gebuehren!
+test()
